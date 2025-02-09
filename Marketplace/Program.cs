91d4ef5300b4bacc735ac44869813ec0800b3908
Dominik <dominikgasior03@gmail.com>
2025-02-09 21:01:46 +0100

    init

---
+++ Marketplace/Program.cs
@@ -0,0 +1,27 @@
+using Scalar.AspNetCore;
+
+namespace Marketplace;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.AddAuthorization();
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+        
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.MapScalarApiReference("/docs");
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+
+        app.Run();
+    }
+}
